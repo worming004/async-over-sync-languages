9adcb4d73e6ca7f7162020b7efde48709382add9
Mathieu Scolas <worming@hotmail.be>
2023-11-21 20:05:16 +0100

    add example for go and csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿Action<string, int> syncConsoleWrite = (string id, int wait) =>
+{
+    System.Console.WriteLine($"Action with id {id} started");
+    Thread.Sleep(wait * 1000);
+    System.Console.WriteLine($"Action with id {id} stopped");
+};
+
+var allTasks = new List<Task>();
+
+const int count = 100000;
+
+
+var start = DateTime.Now;
+for (var i = 0; i < count; i++)
+{
+    var id = i.ToString();
+    allTasks.Add(Task.Run(() =>
+    {
+        syncConsoleWrite(id, 1000);
+    }));
+}
+
+var elapsed = DateTime.Now.Subtract(start);
+await Task.WhenAll(allTasks);
+
+System.Console.WriteLine($"elapsed time {elapsed}");
