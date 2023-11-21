35a4ca0309547c1b0f835043a20f9ee9d23e4dc7
Mathieu Scolas <worming@hotmail.be>
2023-11-21 21:34:34 +0100

    fix csharp

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,14 +1,14 @@
 ﻿Action<string, int> syncConsoleWrite = (string id, int wait) =>
 {
     System.Console.WriteLine($"Action with id {id} started");
-    Thread.Sleep(wait * 1000);
+    Thread.Sleep(wait);
     System.Console.WriteLine($"Action with id {id} stopped");
 };
 
 var allTasks = new List<Task>();
 
-const int count = 100000;
-
+// const int count = 100000; // 100000 is too long ..
+const int count = 200;
 
 var start = DateTime.Now;
 for (var i = 0; i < count; i++)
@@ -20,7 +20,7 @@ for (var i = 0; i < count; i++)
     }));
 }
 
-var elapsed = DateTime.Now.Subtract(start);
 await Task.WhenAll(allTasks);
+var elapsed = DateTime.Now.Subtract(start);
 
 System.Console.WriteLine($"elapsed time {elapsed}");
